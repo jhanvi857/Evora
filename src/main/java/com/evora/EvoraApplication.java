4c8b9b0d28e53531384ecebe0f68e91e68eb07de
jhanvi857 <jhanvip8507@gmail.com>
2026-04-13 12:22:57 +0530

    initial setup

---
+++ src/main/java/com/evora/EvoraApplication.java
@@ -0,0 +1,9 @@
+package main.java.com.evora;
+
+import com.jhanvi857.nioflow.NioFlowApp;
+
+public class EvoraApplication {
+    public static void main(String[] args) {
+        NioFlowApp app = new NioFlowApp();
+    }
+}
